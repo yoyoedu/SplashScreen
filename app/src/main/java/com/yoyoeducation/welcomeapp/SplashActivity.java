44b85934cb1caae1ecb4d9890d338f4a11301a6a
yoyoedu <69307376+yoyoedu@users.noreply.github.com>
2020-08-06 19:35:40 +0700

    SplashScreen
    SplashScreen

---
+++ app/src/main/java/com/yoyoeducation/welcomeapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.yoyoeducation.welcomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // This is Splash Welcome come Screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
